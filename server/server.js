767eb7c3a385e85254a8ca58e79990df9b579ce7
moonkabir <moonkabir4@gmail.com>
2022-10-07 11:06:00 +0600

    install nodemon and solve cors uusing express cors plugin

--- server/server.js
+++ server/server.js
@@ -1,22 +1,19 @@
 import express from "express";
 import mongoose from 'mongoose';
+import cors from 'cors';
 const PORT = 4000;
 const app = express();
+app.use(cors());
 
 // First work when conection work then the DB connected
 mongoose.connect('mongodb+srv://bitfumes_basic_mern:Moon123$@cluster0.32hdevq.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>console.log("MongoDB Connection is successful"))
 .catch((err)=> console.log(err));
 
-// First connect to the server and then other work
-// await mongoose.connect('mongodb+srv://bitfumes_basic_mern:Moon123$@cluster0.32hdevq.mongodb.net/?retryWrites=true&w=majority')
-// .then(()=>console.log("MongoDB Connection is successful"))
-// .catch((err)=> console.log(err));
-
-
 app.get("/",(req,res)=>{
     res.send("Hello World");
 });
+
 app.listen(PORT, ()=>{
     console.log("Server is running at http://localhost:4000");
 })
