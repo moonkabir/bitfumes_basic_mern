8e1f5c20095a90311fb6eacc8fca2359f2cc2b64
moonkabir <moonkabir4@gmail.com>
2022-10-21 08:45:16 +0600

    form design done with material ui

--- client/src/components/TransactionForm.js
+++ client/src/components/TransactionForm.js
@@ -2,16 +2,32 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
 export default function TransactionForm() {
+    function handleChange(){}
   return (
     <Card sx={{ minWidth: 275, marginTop:10 }}>
       <CardContent>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Typography variant="h6"> Add new Transaction</Typography>
+        
+        <form>
+            <TextField sx={{marginRight:5}} size="smaill" id="outlined-basic" label="Amount" variant="outlined" />
+            <TextField sx={{marginRight:5}} size="smaill" id="outlined-basic" label="Description" variant="outlined" />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+                label="Transaction Date"
+                inputFormat="MM/DD/YYYY"
+                onChange={handleChange}
+                renderInput={(params) => <TextField sx={{marginRight:5}} size="smaill" {...params} />}
+            />
+            <Button variant="contained">Submit</Button>
+            </LocalizationProvider>
+        </form>
       </CardContent>
     </Card>
   );
